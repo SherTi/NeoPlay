a25080de5993c3f4d5f76b3fd3210465e06eb56d
SherTi <tulkunovsher69@gmail.com>
2024-05-24 14:25:56 +0500

    fixed movie page

--- src/app/pages/movie-page/movie-page.component.ts
+++ src/app/pages/movie-page/movie-page.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./movie-page.component.css']
 })
 export class MoviePageComponent {
+  stars: number[] = [1,2,3,4,5];
+  selectedValue: number = 0;
+  isMouseover:boolean = true;
 
+  countStar(star: number) {
+    this.isMouseover = false;
+    this.selectedValue = star;
+  }
+
+  addClass(star: number) {
+    if (this.isMouseover) {
+      this.selectedValue = star;
+    }
+  }
+
+  removeClass() {
+    if (this.isMouseover) {
+      this.selectedValue = 0;
+    }
+  }
 }
--- src/app/pages/rate/rate.component.ts
+++ src/app/pages/rate/rate.component.ts
@@ -7,25 +7,6 @@ import { Component } from '@angular/core';
 })
 export class RateComponent {
 
-  stars: number[] = [1,2,3,4,5];
-  selectedValue: number = 0;
-  isMouseover:boolean = true;
 
-  countStar(star: number) {
-    this.isMouseover = false;
-    this.selectedValue = star;
-  }
-
-  addClass(star: number) {
-    if (this.isMouseover) {
-      this.selectedValue = star;
-    }
-  }
-
-  removeClass() {
-    if (this.isMouseover) {
-      this.selectedValue = 0;
-    }
-  }
 
 }
