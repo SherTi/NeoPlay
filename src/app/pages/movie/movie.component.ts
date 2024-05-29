efd7d46ff122b7033f6af31146b2482ce7239996
SherTi <tulkunovsher69@gmail.com>
2024-05-29 23:00:39 +0500

    animations filters

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import {animate, animation, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.css']
+  styleUrls: ['./movie.component.css'],
+  animations: [
+    trigger("filter" , [
+      transition(':enter', [
+        animation([
+          style({
+            transform: "translateY(-50%)",
+          }),
+          animate("0.3s", style({
+            transform: "translateY(0)"
+          }))
+        ])
+      ]),
+      transition(':leave', [
+        animation([
+          style({
+            transform: "translateY(0)",
+          }),
+          animate( "0.3s", style({
+            transform: "translateY(-100%)"
+          }))
+        ])
+      ])
+    ]),
+  ]
 })
 export class MovieComponent {
+  filterSelect: boolean = false;
+  filterSelect1: boolean = false;
+  filterSelect2: boolean = false;
+  filterSelect3: boolean = false;
+  filterSelect4:  boolean = false;
+
 
 }
